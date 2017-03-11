e509240b67a2c96c6fc8ae57c9dfc88a43e581dd
shai <shaidekel1@gmail.com>
2017-03-10 23:57:16 -0500

    HW4. Was unable to get q4 to identify html-minifier and the errors cascaded into the proceeding questions

---
+++ Dekel/run.js
@@ -0,0 +1,21 @@
+var exercise = require('./exercise.js');
+var url = 'http://student.mit.edu/catalog/index.cgi';
+
+exercise.one(url);
+
+exercise.two(url);
+
+exercise.three();
+
+exercise.four();
+/*
+exercise.five();
+
+exercise.six();
+
+exercise.seven();
+
+exercise.eight();
+
+exercise.nine();
+*/
