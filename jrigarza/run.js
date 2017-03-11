1da0832c058da194717f35776f706a3dfa5f6edd
jrigarza <jrgarza@mit.edu>
2017-03-11 00:05:15 -0500

    Submission by Jose Garza

---
+++ jrigarza/run.js
@@ -0,0 +1,21 @@
+var exercise = require('./exercise.js');
+
+var homepage = 'http://student.mit.edu/catalog/index.cgi';
+
+exercise.one(homepage);
+
+exercise.two(homepage);
+
+exercise.three(homepage);
+
+exercise.four(homepage);
+
+exercise.five(homepage);
+
+exercise.six(homepage);
+
+exercise.seven(homepage);
+
+exercise.eight(homepage);
+
+exercise.nine(homepage);
