0c9e557c7bc598b1291673375e0d31e136c42cec
qinziluo <qinziluo@mit.edu>
2017-03-10 20:58:10 -0500

    qinziluo changed it.

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -1,6 +1,13 @@
+var request = require('request');
+var fs = require('fs');
+var shell = require('shelljs/global');
+var htmlminify = require('html-minifier');
+var cheerio = require('cheerio');
 var exercise = {};
 
-exercise.one = function(){
+
+//var name = []
+exercise.one = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -11,9 +18,57 @@ exercise.one = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+
+    var str1 = 'http://student.mit.edu/catalog/m';
+    var str2 = '.html';
+    var course = [['a', 'b', 'c'], ['a', 'b'], ['a'], ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'q', 'r', 's']];
+    var depart = [['1', '2', '6', '11', '16', '22'], ['3', '5', '8', '14', '9', '10', '12', '16', '17', '18', '24', 'STS', 'HST', '21M', '21W'],
+    ['7', '20', '21', 'CC', 'CSB', 'EC', 'EM', 'ES', 'IDS', 'MAS', 'SCM', 'AS', 'MS', 'NS', 'SWE', 'SP', '21A', '21L', 'WGS'],
+    ['4'], ['21G']];
+    var url = [];
+    for (var k = 0; k < depart.length; k++) {
+        for (var i = 0; i < depart[k].length; i++) {
+            for (var j = 0; j < course[k].length; j++) {
+                url.push(str1 + depart[k][i] + course[k][j] + str2);
+                //name.push('m' + depart[k][i] + course[k][j]);
+            }
+        }
+    }
+
+    //console.log(url);
+    return url;
 };
 
-exercise.two = function(){
+exercise.save = function (data, filename) {
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(filename, data, function (err) {
+            if (err) {
+                reject(error);
+            }
+            resolve('The' + filename + 'was saved!');
+        })
+    })
+
+}
+
+exercise.get = function (url) {
+    return new Promise(function (resolve, reject) {
+        function callback(error, response, body) {
+            if (!error) {
+                resolve(body);
+
+            }
+            else {
+                reject(error);
+            }
+        }
+        request(url, callback);
+    })
+
+}
+
+exercise.two = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -26,9 +81,31 @@ exercise.two = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    /*var request = require('shelljs/global');
+    var url = exercise.one();
+    //myObject.CreateFolder ("C:\\users\\qinzi luo\\documents\\");
+    url.forEach(function(u,i){
+        exec('curl ' + u + '>catalog/' + name[i] + '.html');
+    });       
+    */
+    var url = exercise.one();
+    url.forEach(function (url, i) {
+        var page = exercise.get(url);
+        page.then(function (body) {
+            //console.log(body);
+            var filename = 'catalog/' + i + '.html';
+            return exercise.save(body, filename);
+        })
+
+    })
 };
 
-exercise.three = function(){
+exercise.read = function (i) {
+    var content = fs.readFileSync('catalog/' + i + '.html', 'utf-8');
+    content = fs.appendFileSync('catalog/catalog.txt', content);
+}
+
+exercise.three = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -40,9 +117,31 @@ exercise.three = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+
+    var url = exercise.one();
+    /*var done = function () {
+        return new Promise(function (resolve, reject) {
+            exercise.two();
+        });
+    };
+    done().*/
+    for (var i = 0; i < url.length; i++) {
+        exercise.read(i);
+    };
 };
 
-exercise.four = function(){
+
+
+//console.log(str);
+/*var file = new File(txtFile);
+file.open("w"); // open file with write access
+file.writeln(str);
+file.close();*/
+//console.log(str);
+
+
+
+exercise.four = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -55,9 +154,16 @@ exercise.four = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    //console.log('hi');
+
+    var content = fs.readFileSync('catalog/catalog.txt', 'utf-8');
+    content = content.replace(/\n/g, '');
+    content = content.replace(/\r\n/g, '');
+    content = fs.writeFileSync('./catalog/catalog.txt', content);
+
 };
 
-exercise.five = function(){
+exercise.five = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -70,9 +176,15 @@ exercise.five = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    var page = fs.readFileSync('catalog/catalog.txt', 'utf-8');
+    //var $ = cheerio.load(page);
+    var expression = /<h3>(.*?)<br><I>/g;
+    var matches = page.match(expression);
+    //console.log(matches);
+    return matches;
 };
 
-exercise.six = function(){
+exercise.six = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -82,9 +194,17 @@ exercise.six = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    var page = fs.readFileSync('catalog/catalog.txt', 'utf-8');
+    var $ = cheerio.load(page);
+    var matches = [];
+    $('h3').each(function (i, element) {
+        matches.push($(element).text());
+    })
+    //console.log(matches);
+    return matches;
 };
 
-exercise.seven = function(){
+exercise.seven = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -95,9 +215,23 @@ exercise.seven = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    var arr = exercise.six();
+    var words = arr.map(function (title) {
+        return title.toLowerCase().match(/([a-z]+)/g);
+    });
+
+    //console.log(words);
+    for (var i = 0; i < words.length; i++) {
+        words[i] = words[i].filter(function (word) {
+            return word !== 'and' && 'the' && 'a' && 'an' &&'to' &&'of'&&'on'&& 'in';
+        });
+    }
+    //console.log(words);
+    return words;
+
 };
 
-exercise.eight = function(){
+exercise.eight = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -107,9 +241,17 @@ exercise.eight = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    var words = exercise.seven();
+    //console.log('hi');
+    /*words = words.reduce(function (previous, current) {
+        return previous.concat(current);
+    }, [])*/
+    words = [].concat.apply([], words);
+    //console.log(words);
+    return words;
 };
 
-exercise.nine = function(){
+exercise.nine = function () {
     // -----------------------------------------------
     //   YOUR CODE
     //
@@ -119,6 +261,23 @@ exercise.nine = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    var words = exercise.eight();
+    var wordcount= words.reduce(function (previous, current) {
+        if (current !== null) {
+            if (current in previous) {
+                previous[current] += 1;
+            }
+            else {
+                previous[current] = 1;
+            }
+        }
+        return previous;
+
+    }, {})
+    //console.log(wordcount);
+    var content = fs.writeFileSync('./catalogSample/catalog_data.js', 'var scores = '+JSON.stringify(wordcount));
+    return wordcount;
+
 };
 
 
