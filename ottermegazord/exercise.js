956b4a115eb7054e51ce3334aa99cb5ce7f6338c
ottermegazord <idaly.ali@live.fr>
2018-02-25 13:57:18 -0500

    Updated exercise file(s)

--- ottermegazord/exercise.js
+++ ottermegazord/exercise.js
@@ -312,7 +312,7 @@ exercise.seven = function(){
 
     // console.log(courseTitles.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""));
 
-    console.log(words);
+    console.log(common);
     console.log('idaly');
 };
 
