3ed820921c297eb6b2f75108a7cee050652034bf
ottermegazord <idaly.ali@live.fr>
2018-02-25 13:39:52 -0500

    Updated exercise file(s)

--- ottermegazord/exercise.js
+++ ottermegazord/exercise.js
@@ -301,7 +301,7 @@ exercise.seven = function(){
     // //  See homework guide document for more info.
     // // -----------------------------------------------
     var courseTitles = exercise.six();
-    console.log(courseTitles + "666");
+    console.log("666" + courseTitles + "666");
 };
 
 exercise.eight = function(){
