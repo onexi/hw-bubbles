eacf3547839aaba81c8ed9233162332ce45e6ac6
ottermegazord <idaly.ali@live.fr>
2018-02-24 23:35:09 -0500

    Updated exercise file(s)

--- ottermegazord/exercise.js
+++ ottermegazord/exercise.js
@@ -235,6 +235,7 @@ exercise.four = function(){
     var address = '/Users/idaly666/Desktop/onexi/hw-bubbles/ottermegazord/catalog/catalog.txt';
     var input = fs.readFileSync(address, 'utf-8');
     var output = input.replace(/\s/g, "X");
+    fs.writeFileSync(address, output);
     return output;
 };
 
