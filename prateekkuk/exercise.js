228d1108155744df05958ca310e517dbb29a14f1
prateekkuk <prateek.kukreja@gmail.com>
2018-02-26 16:09:42 -0500

    added request.js

--- prateekkuk/exercise.js
+++ prateekkuk/exercise.js
@@ -1,6 +1,8 @@
 var exercise = {};
 var urls = [];
 var fetch = require('node-fetch');
+var cheerio = require('cheerio');
+var request = require('request');
 var fs = require('fs');
 
 exercise.one = function(){
@@ -15,8 +17,7 @@ exercise.one = function(){
     //  See homework guide document for more info.
     // -----------------------------------------------
 
-    var urls = [];
-    
+
 
 
 
