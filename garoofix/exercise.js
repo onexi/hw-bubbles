dfd85cfbc53518d33d2975e1d53416acef25e2ff
garoofix <dgarufi@gmail.com>
2018-03-06 11:46:15 -0500

    Updated exercise file(s)

--- garoofix/exercise.js
+++ garoofix/exercise.js
@@ -13,12 +13,12 @@ exercise.one = function(){
 exercise.two = function(){
     //  Download every course catalog page.
     //  Save every page to "your_folder/catalog"
-    var request = require('request');
-        request('http://www.google.com', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-    });
+    // var request = require('request');
+    //     request('http://www.google.com', function (error, response, body) {
+    //     console.log('error:', error); // Print the error if one occurred
+    //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //     console.log('body:', body); // Print the HTML for the Google homepage.
+    // });
 };
 
 exercise.three = function(){
