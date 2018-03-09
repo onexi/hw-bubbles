4817db12c5d24cf00d76134cd38eaa6315712cd4
deannadelgado <deannad@mit.edu>
2018-03-08 23:20:45 -0500

    Updated exercise file(s)

--- deannadelgado/exercise.js
+++ deannadelgado/exercise.js
@@ -1,5 +1,5 @@
 var request = require('sync-request');
-var minify = require('html-minifer').minify;
+var minify = require('html-minifier').minify;
 var cheerio = require('cheerio');
 var fs = require('fs');
 
@@ -11,32 +11,58 @@ exercise.one = function(){
     
     var urls = [];
     urls.push('http://student.mit.edu/catalog/m1a.html');
+    urls.push('http://student.mit.edu/catalog/m1b.html');
+    urls.push('http://student.mit.edu/catalog/m1c.html');
     urls.push('http://student.mit.edu/catalog/m2a.html');
+    urls.push('http://student.mit.edu/catalog/m2b.html');
+    urls.push('http://student.mit.edu/catalog/m2c.html');
     urls.push('http://student.mit.edu/catalog/m3a.html');
+    urls.push('http://student.mit.edu/catalog/m3b.html');
     urls.push('http://student.mit.edu/catalog/m4a.html');
+    urls.push('http://student.mit.edu/catalog/m4b.html');
+    urls.push('http://student.mit.edu/catalog/m4c.html');
+    urls.push('http://student.mit.edu/catalog/m4d.html');
+    urls.push('http://student.mit.edu/catalog/m4e.html');
+    urls.push('http://student.mit.edu/catalog/m4f.html');
+    urls.push('http://student.mit.edu/catalog/m4g.html');
     urls.push('http://student.mit.edu/catalog/m5a.html');
+    urls.push('http://student.mit.edu/catalog/m5b.html');
     urls.push('http://student.mit.edu/catalog/m6a.html');
+    urls.push('http://student.mit.edu/catalog/m6b.html');
+    urls.push('http://student.mit.edu/catalog/m6c.html');
     urls.push('http://student.mit.edu/catalog/m7a.html');
     urls.push('http://student.mit.edu/catalog/m8a.html');
+    urls.push('http://student.mit.edu/catalog/m8b.html');
     urls.push('http://student.mit.edu/catalog/m9a.html');
+    urls.push('http://student.mit.edu/catalog/m9b.html');
     urls.push('http://student.mit.edu/catalog/m10a.html');
+    urls.push('http://student.mit.edu/catalog/m10b.html');
     urls.push('http://student.mit.edu/catalog/m11a.html');
+    urls.push('http://student.mit.edu/catalog/m11b.html');
+    urls.push('http://student.mit.edu/catalog/m11c.html');
     urls.push('http://student.mit.edu/catalog/m12a.html');
+    urls.push('http://student.mit.edu/catalog/m12b.html');
+    urls.push('http://student.mit.edu/catalog/m12c.html');
     urls.push('http://student.mit.edu/catalog/m14a.html');
+    urls.push('http://student.mit.edu/catalog/m14b.html');
     urls.push('http://student.mit.edu/catalog/m15a.html');
+    urls.push('http://student.mit.edu/catalog/m15b.html');
+    urls.push('http://student.mit.edu/catalog/m15c.html');
     urls.push('http://student.mit.edu/catalog/m16a.html');
-    urls.push('http://student.mit.edu/catalog/m17a.html');
+    urls.push('http://student.mit.edu/catalog/m16b.html');
     urls.push('http://student.mit.edu/catalog/m18a.html');
+    urls.push('http://student.mit.edu/catalog/m18b.html');
     urls.push('http://student.mit.edu/catalog/m20a.html');
     urls.push('http://student.mit.edu/catalog/m22a.html');
-    urls.push('http://student.mit.edu/catalog/m24a.html');
+    urls.push('http://student.mit.edu/catalog/m22b.html');
+    urls.push('http://student.mit.edu/catalog/m22c.html');
     return urls;
 };
 
 exercise.two = function(){
     console.log('|question 2|');
 
-    var urls = excercise.one();
+    var urls = exercise.one();
     urls.forEach(function(url,index){
         var res = request('GET', url);
         var filename = './catalog/' + index + 'html';
@@ -73,15 +99,15 @@ exercise.four = function(){
     });
 
     var clean = scrubbed.replace(/'/g, '');
-    fs.writerFileSync('./catalog/clean.txt', clean);
+    fs.writeFileSync('./catalog/clean.txt', clean);
 };
 
 exercise.five = function(){
     console.log('|question 5|');
 
-    var data = fs.readFileSynce('.catalog/clean.txt');
+    var data = fs.readFileSync('./catalog/clean.txt');
 
-    var $ = cheerio.loud(data);
+    var $ = cheerio.load(data);
 
     var courses = [];
     $('h3').each(function(i,element){
@@ -93,9 +119,9 @@ exercise.five = function(){
 exercise.six = function(){
     console.log('|question 6|');
 
-    var data = fs.readFileSynce('.catalog/clean.txt');
+    var data = fs.readFileSync('./catalog/clean.txt');
 
-    var $ = cheerio.loud(data);
+    var $ = cheerio.load(data);
 
     var titles = [];
     $('h3').each(function(i,element){
@@ -126,6 +152,8 @@ exercise.eight = function(){
 
 exercise.nine = function(){
     console.log('|question 9|');
+
+    var wordsFlat = exercise.eight();
 
     var scores = wordsFlat.reduce(function(previous,current){
         if(current in previous){
