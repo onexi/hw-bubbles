d892b424c03c928298f2b46bf3c958793791c114
jlorrey <jlorrey@mit.edu>
2018-03-08 19:23:28 -0500

    Updated exercise file(s)

--- jlorrey/exercise.js
+++ jlorrey/exercise.js
@@ -16,6 +16,7 @@ exercise.one = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise One");
     var urls = [];
     const course_nums = [1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,18,20,22];
     const a_only = [7, 20];
@@ -62,6 +63,19 @@ exercise.two = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+   
+    //for each elemetn in urls:
+    //  fetch/request the url
+    //  save resulting http to ./catalog
+
+    //Note: I had to create the jlorrey/catalog directory before this would work (seems like it should be able to create the directory on its own)
+    console.log("Exercise Two");
+    var urls = exercise.one();
+    urls.forEach(function(url, index) {
+        var raw_response = request('GET', url);
+        var filename = './catalog/'+index+'.html'; // ./catalog1.html, ./catalog2.html, ...
+        fs.writeFileSync(filename, raw_response.getBody().toString()); //save the body of the html response (as a string) to the given filename
+    });
 };
 
 exercise.three = function(){
@@ -76,8 +90,17 @@ exercise.three = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise Three");
+    var myFiles = [];
+    for (var i=0; i<46; i++) { //get files 0 through 45
+        myFiles.push('./catalog/'+i+'.html');
+    };
+    myFiles.forEach(function(file, index) {
+        var file_data = fs.readFileSync(file);
+        // console.log('Adding'+file+" to catalog.txt");
+        fs.appendFileSync('./catalog/catalog.txt', file_data);
+    });
 };
-
 exercise.four = function(){
     // -----------------------------------------------
     //   YOUR CODE
@@ -91,6 +114,15 @@ exercise.four = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise Four");
+    var myData = fs.readFileSync('./catalog/catalog.txt');
+    var scrubbed=minify(myData.toString(), {
+        collapseWhitespace: true,
+        minifyJS          : true,
+        minifyCSS         : true
+    });
+    var cleaned = scrubbed.replace(/'/g, ''); //remove all (/g = global) single quotes you see
+    fs.writeFileSync('./catalog/clean.txt', cleaned); //write the cleaned data to a clean.txt file
 };
 
 exercise.five = function(){
@@ -106,6 +138,15 @@ exercise.five = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise Five");
+    var myData = fs.readFileSync('./catalog/clean.txt');
+    var $ = cheerio.load(myData);
+    var courses = [];
+    $('h3').each(function(i, element) { //get elements with the h3 tag
+        courses.push($(element).text()); //and push them onto the courses array
+    });
+    // console.log("COURSES:", courses); //prints out all course names (eg. '1.232[J]: The Airline Industry')
+    return courses;
 };
 
 exercise.six = function(){
@@ -118,6 +159,17 @@ exercise.six = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise Six");
+    var myData = fs.readFileSync('./catalog/clean.txt');
+    var $ = cheerio.load(myData);
+    var titles = [];
+    $('h3').each(function(i, element) {
+        titles.push($(element).text()); //title = text part of course name
+    });
+    // console.log(titles);
+    return titles;
+
+    //Isn't this the exact same thing as question 5?
 };
 
 exercise.seven = function(){
@@ -131,6 +183,14 @@ exercise.seven = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise Seven");
+    var titles = exercise.six();
+    // gets word array from titles & filters out all non-letters (e.g. punctuation & numbers)
+    var words = titles.map(function(title) {
+        return title.toLowerCase().match(/([a-z]+)/g); // /g = global, /[a-z] = all letters a-z
+    }); //map calls a function on every element of an existing array to create a new array 
+    // console.log(words);
+    return words;
 };
 
 exercise.eight = function(){
@@ -143,6 +203,12 @@ exercise.eight = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise Eight");
+    var words = exercise.seven();
+    var wordsFlat = words.reduce(function(previous, current) {
+        return previous.concat(current);
+    }, []);
+    return wordsFlat;
 };
 
 exercise.nine = function(){
@@ -155,6 +221,22 @@ exercise.nine = function(){
     //
     //  See homework guide document for more info.
     // -----------------------------------------------
+    console.log("Exercise Nine");
+    var wordsFlat = exercise.eight();
+    //use reduce to count word freq
+    var scores = wordsFlat.reduce(function(previous, current){
+        if (current != 'j') { //ignore J (turned lowercase) from joint courses
+            if (current in previous) { //we've come across this word before (f>=1)
+                previous[current] += 1;
+            }
+            else { //first instance of this word
+                previous[current] = 1;
+            }
+        }
+        return previous;
+    }, {});
+    console.log(scores);
+    return scores;
 };
 
 
